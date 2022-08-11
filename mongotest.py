a59dc00d0515b3801b91fd08ceac7556a094f96f
saurabhdata <saurabhdata.python@gmail.com>
2022-08-11 18:25:44 +0530

    test connection of mongodb with create dict

---
+++ mongotest.py
@@ -0,0 +1,12 @@
+import  pymongo
+client = pymongo.MongoClient("mongodb+srv://saurabhdata:Rajbhatta1!#$@saurabhdata.ckhjdot.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name" : "saurabh",
+    "last name " : "kumar",
+    "mobile_no" : 766581990
+}
+db1 = client["mongotest"]
+coll = db1["test"]
+coll.insert_one(d)
